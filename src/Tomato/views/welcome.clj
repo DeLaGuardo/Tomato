96607c03529c9e7b4ced58cd119c8e6860bc9090
DeLaGuardo <delaguardo@gmail.com>
2012-04-09 11:05:23 +0400

    initialize new noir project

---
+++ src/Tomato/views/welcome.clj
@@ -0,0 +1,9 @@
+(ns Tomato.views.welcome
+  (:require [Tomato.views.common :as common]
+            [noir.content.getting-started])
+  (:use [noir.core :only [defpage]]
+        [hiccup.core :only [html]]))
+
+(defpage "/welcome" []
+         (common/layout
+           [:p "Welcome to Tomato"]))
